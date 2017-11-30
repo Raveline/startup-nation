1f0a888d918ec168a15abd5fab01cc7f32462b86
Thomas Crevoisier <thomas.crevoisier@fretlink.com>
2017-11-30 13:53:40 +0100

    Basic rendering with Pux

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,31 @@
 module Main where
 
-import Prelude
+import Prelude hiding (div)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Pux (CoreEffects, EffModel, start, noEffects)
+import Pux.DOM.HTML (HTML)
+import Pux.Renderer.React (renderToDOM)
+import Text.Smolder.HTML (div)
+import Text.Smolder.Markup (text)
+
+type State = Int
+type Event = Unit
+
+foldp :: forall fx. Event -> State -> EffModel State Event fx
+foldp _ s = noEffects s
+
+initialState :: State
+initialState = 0
+
+view :: State -> HTML Event
+view _ = div (text "plop")
+
+main :: forall fx. Eff (CoreEffects fx) Unit
 main = do
-  log "Hello sailor!"
+  app <- start { initialState
+               , view
+               , foldp
+               , inputs: []
+               }
+  renderToDOM "#app" app.markup app.input
